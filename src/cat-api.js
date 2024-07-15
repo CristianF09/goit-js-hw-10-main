8d10a739aa07d65af5747280d0fb8b7cd479fab8
CristianF09 <cris7i_laurentiu@yahoo.com>
2024-07-15 21:48:00 +0300

    UpdateError3

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,25 +1,26 @@
 import axios from 'axios';
 
-
 axios.defaults.headers.common['x-api-key'] = 'live_DZEVyJW3VcBIexNIe3baRDqWHCXVAhgw8tJCASx5USwQZM7mdoM34w9t2SuiEjMc';
 
-@returns {Promise<Array>}
+ @returns {Promise<Array>}
+ @throws 
 
 export const fetchBreeds = async () => {
   try {
-   const response = await axios.get('https://api.thecatapi.com/v1/breeds123');
+    const response = await axios.get('https://api.thecatapi.com/v1/breeds');
     return response.data;
   } catch (error) {
     throw new Error('Failed to fetch breeds');
   }
 };
 
-@param {string} breedId 
-@returns {Promise<Object>} 
-
+ @param {string} breedId 
+ @returns {Promise<Object>} 
+ @throws 
+ 
 export const fetchCatByBreed = async (breedId) => {
   try {
-    const response = await axios.get(`https://api.thecatapi.com/v1/images/search123?breed_ids=${breedId}`);
+    const response = await axios.get(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`);
     return response.data[0];
   } catch (error) {
     throw new Error('Failed to fetch cat by breed');
